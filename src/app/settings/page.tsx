53b7f337e25cbace2f4bde87e77ce0285f3ff593
NielsPrins <NielsPrins@users.noreply.github.com>
2023-09-22 11:31:50 +0200

    force dynamic

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,6 +2,8 @@ import getMongoCollection from '@/src/db';
 import Setting from '@/src/database/models/setting';
 import Settings from '@/src/app/settings/settings';
 
+export const dynamic = 'force-dynamic';
+
 async function getCalendarApiToken() {
   const settingsCollection = await getMongoCollection('settings');
   const calendarApiTokenSetting = await settingsCollection.findOne<Setting>({ key: 'calendarApiToken' });
