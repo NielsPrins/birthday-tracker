3405b3bb3204867a423ee84e9d1721e8885c9441
NielsPrins <NielsPrins@users.noreply.github.com>
2023-09-23 18:53:18 +0200

    Last synced

--- src/app/calendar/route.ts
+++ src/app/calendar/route.ts
@@ -42,7 +42,7 @@ export async function GET(req: NextRequest) {
       start: [nextBirthday.getUTCFullYear(), nextBirthday.getUTCMonth(), nextBirthday.getUTCDate()],
       end: [nextBirthday.getUTCFullYear(), nextBirthday.getUTCMonth(), nextBirthday.getUTCDate() + 1],
       title: `${birthday.name}${newAge ? ` (${newAge})` : ''}`,
-      description: `Last sync on: ${moment(now).format('DD MMMM YYYY HH:mm')}`,
+      description: `Last synced: ${moment(now).fromNow()}`,
     };
   });
 
